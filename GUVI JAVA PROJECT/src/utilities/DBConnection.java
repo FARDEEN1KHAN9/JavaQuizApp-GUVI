3e7e5f266bef81a1561ee0baeea2a1e3f1cd01f8
FARDEEN1KHAN9 <fardeen.24sece1290012@galgotiasuniversity.ac.in>
2025-06-08 17:28:27 +0530

    Update DBConnection.java

--- GUVI JAVA PROJECT/src/utilities/DBConnection.java
+++ GUVI JAVA PROJECT/src/utilities/DBConnection.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/quizguvi";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "P@ssguviquiz";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
